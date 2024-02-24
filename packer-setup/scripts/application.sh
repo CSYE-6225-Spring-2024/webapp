91f8c42417a21d7dbf8695f448df2acb0818b682
Anirban Dutta <dutta.an@northeastern.edu>
2024-02-23 23:27:15 -0500

    Revert changes for demo assignment 4

--- packer-setup/scripts/application.sh
+++ packer-setup/scripts/application.sh
@@ -5,12 +5,12 @@ sudo unzip /tmp/webapp.zip -d /opt/webapp
 
 cd /opt/webapp/ || exit 
 
-# sudo tee -a .env <<EOF >/dev/null
-# DB_NAME=${DB_NAME}
-# DB_PWD=${DB_PWD}
-# DB_USER=${DB_USER}
-# DB_PORT=$((DB_PORT))
-# EOF
+sudo tee -a .env <<EOF >/dev/null
+DB_NAME=${DB_NAME}
+DB_PWD=${DB_PWD}
+DB_USER=${DB_USER}
+DB_PORT=$((DB_PORT))
+EOF
 
 sudo chown csye6225:csye6225 /opt/webapp/ -R
  

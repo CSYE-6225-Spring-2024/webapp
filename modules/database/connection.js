db650277a4099ca6a685d85e34c9cfac7e020d7a
Anirban Dutta <dutta.an@northeastern.edu>
2024-02-27 09:24:44 -0500

    Remove default ENVs from webapp

--- modules/database/connection.js
+++ modules/database/connection.js
@@ -2,11 +2,12 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || "test_database",
-  process.env.DB_USER || "user",
-  process.env.DB_PWD || "cloud_2024",
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PWD,
   {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "postgres",
     logging: false,
   }

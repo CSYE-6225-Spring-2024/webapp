4e0dea1c4942feca09144960b2ea522118c10b37
Anirban Dutta <dutta.an@northeastern.edu>
2024-02-27 00:06:21 -0500

    Remove DB dependencies
    Remove postgres setup and installation from packer
    Introduce ENVs in application

--- packer-setup/scripts/dependency.sh
+++ packer-setup/scripts/dependency.sh
@@ -2,6 +2,4 @@
 
 sudo dnf module install nodejs:18/common -y
 
-sudo dnf module install postgresql:16/server -y
-
 sudo dnf install unzip -y
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require("./api/app.js");
-const server = app.listen(8080, function () {
-  console.log("Listening on port 8080");
+const server = app.listen(process.env.WEB_PORT || 8080, function () {
+  console.log("Listening on port:", process.env.WEB_PORT);
 });
 
 module.exports = { server, app };

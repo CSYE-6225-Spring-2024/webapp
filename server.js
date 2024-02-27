482053501a893250a4d1d9ea89dc6069d97f22ae
Anirban Dutta <dutta.an@northeastern.edu>
2024-02-27 09:24:44 -0500

    Remove default ENVs from webapp

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var app = require("./api/app.js");
-const server = app.listen(process.env.WEB_PORT || 8080, function () {
+const server = app.listen(process.env.WEB_PORT, function () {
   console.log("Listening on port:", process.env.WEB_PORT);
 });
 

843125ad9f123a5357558eca3cd1041e37250d03
Anirban Dutta <dutta.an@northeastern.edu>
2024-03-17 17:19:59 -0400

    Introduce winston for logger
    Logs provided in POST request, and changes in config.yaml
    for ops agent config

--- packer-setup/scripts/monitoring.sh
+++ packer-setup/scripts/monitoring.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+sudo mkdir -p /var/log/webapp/
+
+touch /var/log/webapp/webapp.log
+
+sudo chown csye6225:csye6225 /var/log/webapp/ -R
+
 sudo curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install

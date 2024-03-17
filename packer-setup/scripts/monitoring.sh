4b6dbbe8fba899043a464625ba9a6d172013c14c
Anirban Dutta <dutta.an@northeastern.edu>
2024-03-16 22:13:02 -0400

    Add monitoring google ops in packer

---
+++ packer-setup/scripts/monitoring.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+
+sudo cp /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
+
+sudo systemctl restart google-cloud-ops-agent

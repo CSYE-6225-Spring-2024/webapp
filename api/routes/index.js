f1d8015356c48348b200b3657fa0c3a547b9e60a
Anirban Dutta <dutta.an@northeastern.edu>
2024-04-10 10:37:16 -0400

    Change version to v1 and make replacement method as ENV

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,7 @@ var authenticate = require("./authenticate.js");
 
 const routes = (app) => {
   app.use("/healthz", healthz);
-  app.use("/v2/user", authenticate);
+  app.use("/v1/user", authenticate);
   app.all("/*", (req, res) => res.status(404).send());
 };
 
